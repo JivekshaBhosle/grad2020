8bfa3e5e73531536ff6784dbf894b6ba936d9934
Jack.LiuKui <jack.liukui@takealot.com>
2020-02-05 14:38:07 +0200

    udpate view

--- Presentation/ProductView/ProductView.swift
+++ Presentation/ProductView/ProductView.swift
@@ -13,17 +13,16 @@ struct ProductView: View {
             // Upper Stackview - Contains Horizontal stack and post content
             VStack(alignment: .leading, spacing: 15.0) {
                 HStack(spacing: 60) {
-                    // avatar
-                    // AvatarView(image: post.user.avatar, size: 70)
+
                     VStack(alignment: .leading) {
                         
                         Text(viewModel.data.title).font(.body)
-                            .padding(.leading, 5)
-                            .padding(.top, 5)
+                            .padding(EdgeInsets.init(top: 5, leading: 5, bottom: 0, trailing: 0))
+                        
                         
                         Text(viewModel.data.subtitle ?? "").lineLimit(nil).font(.caption)
-                            .padding(.leading, 5)
-                            .padding(.top, 5)
+                            .padding(EdgeInsets.init(top: 5, leading: 5, bottom: 0, trailing: 0))
+                        
                     }
                 }
                 ImageView.init(url: viewModel.data.full)
@@ -32,36 +31,9 @@ struct ProductView: View {
             .background(Color.orange)
             .cornerRadius(5)
             .scaledToFill()
-                .padding(5)  // spacing from left edge of the view
-                .padding(.trailing, 5)// spacing from right edge of the view
+            .padding(EdgeInsets.init(top: 0, leading: 5, bottom: 0, trailing: 5))
             
             
-            
-            
-            //            URLImage(URL.init(string: viewModel.data.full)!, placeholder: {
-            //                ProgressView($0) { progress in
-            //                    ZStack {
-            //                        if progress > 0.0 {
-            //                            // The download has started. CircleProgressView displays the progress.
-            //                            CircleProgressView(progress).stroke(lineWidth: 8.0)
-            //                        }
-            //                        else {
-            //                            // The download has not yet started. CircleActivityView is animated activity indicator that suits this case.
-            //                            CircleActivityView().stroke(lineWidth: 50.0)
-            //                        }
-            //                    }
-            //                }
-            //                    .frame(width: 50.0, height: 50.0)
-            //            })
-            
-            //            // post image
-            //            URLImage(url: URL.init(string: viewModel.data.full))
-            //                .frame(minWidth: 100.0, maxWidth: 100.0, minHeight: 100.0, maxHeight: 100.0)
-            //                .clipped()
-            //            Image(post.image ?? "")
-            //                .resizable()  // creates resizable image
-            //                // this will allow image to fit the screen width maintaining aspect ratio
-            //                .aspectRatio(3/2, contentMode: .fit)
         }
     }
 }
